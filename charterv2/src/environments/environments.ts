d45533f03ce4af474bf991fd6c45d9eaeb265947
Valeria Durruty <104698500+ValeriaDurruty@users.noreply.github.com>
2025-05-09 10:37:44 -0300

    Se borran keys.

--- charterv2/src/environments/environments.ts
+++ charterv2/src/environments/environments.ts
@@ -1,12 +1,6 @@
 export const environment = {
     firebaseConfig: {
-        apiKey: "AIzaSyBmf5LQe1yqF_QQduzPo_BvaRhCdX3gHX4",
-        authDomain: "easyride-3306a.firebaseapp.com",
-        projectId: "easyride-3306a",
-        storageBucket: "easyride-3306a.appspot.com",
-        messagingSenderId: "409674269171",
-        appId: "1:409674269171:web:1e7f4a1e68c7c9fb32d286",
-        measurementId: "G-2ZPNGCK20M"
+        
       },
     production: false,
     endopoint: 'http://localhost:3000/'
